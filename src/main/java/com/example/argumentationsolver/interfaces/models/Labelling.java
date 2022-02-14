5cf0daf135362990d215dfe1c7200a327b6617ac
Muhammad Haris Yousaf <shyamkakkad001@gmail.com>
2022-02-14 21:58:05 +0500

    Initial

--- src/main/java/com/example/argumentationsolver/interfaces/models/Labelling.java
+++
@@ -1,8 +0,0 @@
-package com.example.argumentationsolver.interfaces.models;
-
-/**
- * Represents a labelling relating to the acceptance of an argument in an argumentation framework
- */
-public enum Labelling {
-    IN, OUT, UNDECIDED
-}
